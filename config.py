4e3d7e63718b2d4896e2254048eed6effc7734c7
maxkuo625 <55643879+maxkuo625@users.noreply.github.com>
2025-06-12 16:12:54 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-CHANNEL_ACCESS_TOKEN = "zsCfcsfU5LV183DAvovGD2zXq3i21kHim4ZjFrR/Hlb0P7r30Za15yML0s+XdTw2P0349zpiak1O+/kR5q3Pgr6DFto7L6KZnTjiXFsu2SgrWg92nsh6a9Q/67M+mAQv76jFkjVu+LzJR+enkqbEZwdB04t89/1O/w1cDnyilFU="
-CHANNEL_SECRET = "ff4186c379a7adbfe902eccc62f89505"
-LIFF_ID = '2007541587-XzE0k170'
+CHANNEL_ACCESS_TOKEN = "CHANNEL_ACCESS_TOKEN"
+CHANNEL_SECRET = "CHANNEL_SECRET"
+LIFF_ID = 'LIFF_ID'
 
 DB_CONFIG = {
-    'user': 'root',
-    'password': 'maxkuo5584240',
+    'user': 'user',
+    'password': 'password',
     'host': 'localhost',
     'database': 'line_medbot',
 }
